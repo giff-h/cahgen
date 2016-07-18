d866d49850afe42ff626f5c6490701bac9d91781
Hameed Gifford <giff.h92@gmail.com>
2016-07-18 14:39:06 -0400

    Added image size handler, can now pull from any image. Improved option organizing, all hard-coding should now be removed.

--- gen.py
+++ gen.py
@@ -1,3 +1,5 @@
+from img_size import get_image_size
+
 from copy import deepcopy
 
 from reportlab.lib.pagesizes import letter
@@ -7,34 +9,51 @@ from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import Paragraph
 
 page_width, page_height = letter
-
-card_start_top = 0.25 * inch
-card_start_side = 0.5 * inch
-card_height = 3.5 * inch
-card_width = 2.5 * inch
-card_stop_side = card_start_side + card_width * 3
-card_stop_bottom = card_start_top + card_height * 3
-
-text_offset_x = 11.0
-text_offset_y = 11.0
-
 front = deepcopy(getSampleStyleSheet()["Normal"])
-front.fontSize = 15
-front.leading = 18
-
 back = deepcopy(getSampleStyleSheet()["Normal"])
 back.fontName = "Helvetica-Bold"
-back.fontSize = 35
-back.leading = 42
 
-img_x = 30
-img_y = 21
-
-blank = "_" * 5
-
-phrase_fs = 7
+# Options
+card_width = 2.5
+card_height = 3.5
+text_offset_x = 10.0
+text_offset_y = 10.0
+blank = 5
 phrase = "Calling All Heretics"
+front.fontSize = 14
+back.fontSize = 35
+img_name = "cards.png"
+img_w = 30
+
+# Defining parameters from options
+card_width *= inch
+card_height *= inch
+
+cards_wide = int(page_width // card_width)
+cards_high = int(page_height // card_height)
+
+card_start_top = (page_height - card_height * cards_high) / 2
+card_start_side = (page_width - card_width * cards_wide) / 2
+
+card_stop_side = card_start_side + card_width * cards_wide
+card_stop_bottom = card_start_top + card_height * cards_high
+
+grid = cards_wide * cards_high
+
+blank = "_" * blank
+
+# Remaining as true as possible to the ratio of the image.
+src_img = get_image_size(img_name)
+if src_img:
+    src_img_w, src_img_h = src_img
+    img_h = round(img_w * src_img_h / src_img_w)
+else:
+    img_h = img_w
+
 assert ''.join(i[0] for i in phrase.split()) == "CAH"
+phrase_font_size = 7
+front.leading = round(front.fontSize * 1.2)
+back.leading = round(back.fontSize * 1.2)
 
 
 def not_special(card):
@@ -53,16 +72,16 @@ def process_black(card):
     card = process_card(card)
     processed = blank if card.startswith("_") else ""
     processed += blank.join([x for x in card.split("_") if x])
-    return processed + blank if card.endswith("_") else processed
+    return "<b>{}</b>".format(processed + blank if card.endswith("_") else processed)
 
 
 def draw_grid(pdf: Canvas):
-    for x in range(4):
+    for x in range(cards_wide + 1):
         line_x = card_start_side + card_width * x
         line_y_b = page_height - card_start_top
         line_y_e = page_height - card_stop_bottom
         pdf.line(line_x, line_y_b, line_x, line_y_e)
-    for y in range(4):
+    for y in range(cards_high + 1):
         line_x_b = card_start_side
         line_x_e = card_stop_side
         line_y = page_height - (card_start_top + card_height * y)
@@ -70,12 +89,12 @@ def draw_grid(pdf: Canvas):
 
 
 def write_page(pdf: Canvas, page, image=True):
-    pdf.setFont("Helvetica-Bold", phrase_fs)
-    for row_i in range(0, len(page), 3):
-        row = page[row_i:row_i+3]
+    pdf.setFont("Helvetica-Bold", phrase_font_size)
+    for row_i in range(0, len(page), cards_wide):
+        row = page[row_i:row_i + cards_wide]
         for i in range(len(row)):
             start_x = card_start_side + card_width * i
-            start_y = card_start_top + card_height * (row_i // 3)
+            start_y = card_start_top + card_height * (row_i // cards_wide)
 
             end_x = start_x + card_width
             end_y = start_y + card_height
@@ -89,8 +108,8 @@ def write_page(pdf: Canvas, page, image=True):
             end_y = page_height - end_y
 
             if image:
-                pdf.drawImage("cards.png", start_x, end_y, img_x, img_y)
-                pdf.drawString(start_x + img_x + 5, end_y + img_y // 2 - phrase_fs // 2, phrase)
+                pdf.drawImage("cards.png", start_x, end_y, img_w, img_h)
+                pdf.drawString(start_x + img_w + 5, end_y + img_h // 2 - phrase_font_size // 2, phrase)
 
             card_p = row[i] if isinstance(row[i], Paragraph) else Paragraph(row[i], front)
             size = card_p.wrap(abs(end_x - start_x), abs(end_y - start_y))
@@ -103,9 +122,9 @@ def write_file(cards, filename):
         return
     file = Canvas(filename, pagesize=letter)
     try:
-        for page_start in range(0, len(cards), 9):
+        for page_start in range(0, len(cards), grid):
             draw_grid(file)
-            write_page(file, cards[page_start:page_start + 9])
+            write_page(file, cards[page_start:page_start + grid])
     finally:
         file.save()
 
